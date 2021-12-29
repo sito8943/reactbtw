c47f70d3bb612579d198e73aa6ea38a05e79fe3d
Sito8943 <sito8943@gmail.com>
2021-12-29 10:48:24 +0100

    working with react-icons

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import settings from "../../img/compass.svg";
+// 3d libs
+import { MdSettings, MdMenu, MdMenuOpen } from "react-icons/md";
+
 
 // styles
 import "./style.scss";
 
 const TopBar = (props) => {
   const { texts } = props;
+  const [ toggleMenu, setToggleMenu ] = useState(false);
+
+  const toggleMenuHandler = () => {
+    setToggleMenu(!toggleMenu);
+  }
 
   return (
-    <div className="top-bar flex justify-space-between align-center">
-      <button>
-        <img src={settings} alt="settings-gear" />
-      </button>
-      <button>
-        <img src={settings} alt="menu-bars" />
-      </button>
-    </div>
+    <>
+      <div className="top-bar flex justify-left align-center">
+        <button>
+          <MdSettings />
+        </button>
+        <button onClick={toggleMenuHandler}>
+        {toggleMenu ? <MdMenu /> : <MdMenuOpen />}
+        </button>
+      </div>
+      <div>
+      </div>
+    </>
   );
 };
 
