664029dc39689a7aad5aff9f6643c47f911ae612
sito8943 <sito8943@gmail.com>
2022-04-29 23:39:25 -0400

    classes

--- src/models/Classes.js
+++ src/models/Classes.js
@@ -4,3 +4,16 @@ export const ClassEnumType = {
   Mage: 2,
   Defender: 3,
 };
+
+export const CreationClass = {
+  0: {
+    Attack: 10,
+    Life: 20,
+    Mana: 15,
+    Armor: 1,
+    Luck: 2,
+  },
+  1: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
+  2: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
+  3: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
+};
