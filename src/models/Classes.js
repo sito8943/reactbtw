30da21ae012d134088bca5ab87cb253d267dfe82
sito8943 <sito8943@gmail.com>
2022-04-29 23:40:58 -0400

    classes

--- src/models/Classes.js
+++ src/models/Classes.js
@@ -13,7 +13,7 @@ export const CreationClass = {
     Armor: 1,
     Luck: 2,
   },
-  1: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
-  2: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
-  3: { Attack: 10, Life: 20, Mana: 15, Armor: 1, Luck: 2 },
+  1: { Attack: 15, Life: 15, Mana: 15, Armor: 1, Luck: 3 },
+  2: { Attack: 20, Life: 15, Mana: 25, Armor: 1, Luck: 2 },
+  3: { Attack: 10, Life: 30, Mana: 10, Armor: 2, Luck: 1 },
 };
