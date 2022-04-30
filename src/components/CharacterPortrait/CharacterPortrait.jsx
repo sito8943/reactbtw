938783ba5b5913e06e8097c2f5618983e72a4355
sito8943 <sito8943@gmail.com>
2022-04-29 20:32:10 -0400

    character portrait animations

--- src/components/CharacterPortrait/CharacterPortrait.jsx
+++ src/components/CharacterPortrait/CharacterPortrait.jsx
@@ -33,7 +33,13 @@ const CharacterPortrait = (props) => {
   const { contextState } = useContext();
   const { reward, isAnimating } = useReward("rewardId", "confetti");
 
+  // see more states for animation
   const [seeMore, setSeeMore] = useState(false);
+  const [portraitAnimation, setPortraitAnimation] = useState(false);
+  const [seeMoreAnimation, setSeeMoreAnimation] = useState(false);
+  const [attributesAnimation, setAttributesAnimation] = useState(false);
+
+  const [appearDone, setAppearDone] = useState(false);
   const [characterName, setCharacterName] = useState("");
   const [nameFocus, setNameFocus] = useState(false);
 
@@ -42,7 +48,7 @@ const CharacterPortrait = (props) => {
 
     background: "#222222",
     border: "1px solid #383838",
-    borderRadius: !seeMore ? "1rem" : "1rem 0 0 1rem",
+    borderRadius: !portraitAnimation ? "1rem" : "1rem 0 0 1rem",
     width: "200px",
     height: "300px",
     zIndex: 1,
@@ -50,6 +56,7 @@ const CharacterPortrait = (props) => {
       color: "#aeb4b9",
     },
     color: "#5e6264",
+    transform: !portraitAnimation ? "translateX(0)" : "translateX(-20px)",
     transition: "all 400ms ease",
   };
 
@@ -98,16 +105,16 @@ const CharacterPortrait = (props) => {
   });
 
   const attributes = {
-    opacity: seeMore ? 1 : 0,
+    opacity: 1,
     padding: "10px",
     border: "1px solid #383838",
     borderRadius: "0 1rem 1rem 0",
     background: "#222222eb",
-    transform: !seeMore ? "translateX(-200px)" : "translateX(0)",
+    width: portraitAnimation ? "400px" : "0",
     height: "300px",
     transition: "all 1000ms ease",
     marginLeft: "-20px",
-    paddingLeft: "20px",
+    paddingLeft: portraitAnimation ? "20px" : 0,
     overflow: "auto",
     color: "#797e81",
   };
@@ -136,8 +143,45 @@ const CharacterPortrait = (props) => {
   });
 
   useEffect(() => {
-    setTimeout(() => document.getElementById("click").click(), 5000);
+    const time = setTimeout(() => {
+      setAppearDone(true);
+    }, 5000);
+    document.body.onkeydown = () => {
+      clearTimeout(time);
+      setAppearDone(true);
+    };
+    return () => {
+      document.body.onkeydown = null;
+    };
   }, []);
+
+  useEffect(() => {
+    if (appearDone) document.getElementById("click").click();
+  }, [appearDone]);
+
+  const toggleSeeMore = () => {
+    if (seeMore) {
+      setAttributesAnimation(false);
+      setTimeout(() => {
+        setPortraitAnimation(false);
+        setSeeMoreAnimation(!seeMoreAnimation);
+        setTimeout(() => {
+          setSeeMore(!seeMore);
+        }, 1000);
+      }, 400);
+    } else {
+      setSeeMore(true);
+      setTimeout(() => {
+        setPortraitAnimation(true);
+        setTimeout(() => {
+          setSeeMoreAnimation(!seeMoreAnimation);
+          setTimeout(() => {
+            setAttributesAnimation(true);
+          }, 200);
+        }, 1000);
+      }, 5);
+    }
+  };
 
   return (
     <>
@@ -157,7 +201,7 @@ const CharacterPortrait = (props) => {
             id={id}
             name={name}
             style={style}
-            className="rotate-card"
+            className={appearDone ? "" : "rotate-card"}
           >
             <Container
               sx={{
@@ -170,10 +214,12 @@ const CharacterPortrait = (props) => {
             >
               <button
                 className={moreButton}
-                onClick={() => setSeeMore(!seeMore)}
+                onClick={toggleSeeMore}
+                disabled={appearDone ? false : true}
               >
                 <MdAddCircle
                   style={{
+                    opacity: appearDone ? 1 : 0,
                     transition: "all 400ms ease",
                     transform: seeMore ? "rotateZ(45deg)" : "rotateX(0deg)",
                   }}
@@ -239,132 +285,139 @@ const CharacterPortrait = (props) => {
               </Container>
             </Container>
           </Container>
-          <Container sx={attributes}>
-            <Container
-              flexDirection="column"
-              sx={{
-                overflow: "auto",
-                padding: "0 20px",
-                width: "100%",
-                opacity: seeMore ? 1 : 0,
-                transition: seeMore ? "all 1000ms ease" : "none",
-              }}
-            >
-              <h3>{languageState.texts.CharacterPortrait.Labels.Attributes}</h3>
-              <Container>
-                <Container flexDirection="column">
-                  <Container
-                    sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
-                    id="attack-row"
-                    extraProps={{
-                      "data-tip": seeMore
-                        ? languageState.texts.CharacterPortrait.Tooltips.Attack
-                        : "",
-                    }}
-                  >
-                    <span style={{ marginRight: "20px" }} className={label}>
-                      {AttributeIcons.attack}
-                    </span>
-                    <Container sx={{ marginBottom: "2px" }}>
-                      <span className={label}>
-                        {languageState.texts.CharacterPortrait.Labels.Attack}{" "}
+          {seeMore && (
+            <Container sx={attributes}>
+              <Container
+                flexDirection="column"
+                sx={{
+                  overflow: "auto",
+                  padding: "0 20px",
+                  width: "100%",
+                  display: !seeMoreAnimation ? "none" : "flex",
+                  opacity: attributesAnimation ? 1 : 0,
+                  transition: "all 400ms ease",
+                }}
+              >
+                <h3>
+                  {languageState.texts.CharacterPortrait.Labels.Attributes}
+                </h3>
+                <Container>
+                  <Container flexDirection="column">
+                    <Container
+                      sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
+                      id="attack-row"
+                      extraProps={{
+                        "data-tip": seeMoreAnimation
+                          ? languageState.texts.CharacterPortrait.Tooltips
+                              .Attack
+                          : "",
+                      }}
+                    >
+                      <span style={{ marginRight: "20px" }} className={label}>
+                        {AttributeIcons.attack}
                       </span>
-                      <span>{contextState.attack}</span>
+                      <Container sx={{ marginBottom: "2px" }}>
+                        <span className={label}>
+                          {languageState.texts.CharacterPortrait.Labels.Attack}{" "}
+                        </span>
+                        <span>{contextState.attack}</span>
+                      </Container>
+                    </Container>
+                    <Container
+                      sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
+                      id="armor-row"
+                      extraProps={{
+                        "data-tip": seeMoreAnimation
+                          ? languageState.texts.CharacterPortrait.Tooltips.Armor
+                          : "",
+                      }}
+                    >
+                      <span className={label}>{AttributeIcons.armor}</span>
+                      <Container sx={{ marginBottom: "2px" }}>
+                        <span className={label}>
+                          {languageState.texts.CharacterPortrait.Labels.Armor}{" "}
+                        </span>
+                        <span>{contextState.armor}</span>
+                      </Container>
+                    </Container>
+                    <Container
+                      sx={{ ...portraitRow, ...bigFont }}
+                      id="luck-row"
+                      extraProps={{
+                        "data-tip": seeMoreAnimation
+                          ? languageState.texts.CharacterPortrait.Tooltips.Luck
+                          : "",
+                      }}
+                    >
+                      <span style={{ marginRight: "20px" }}>
+                        {AttributeIcons.luck}
+                      </span>
+                      <Container sx={{ marginBottom: "2px" }}>
+                        <span className={label}>
+                          {languageState.texts.CharacterPortrait.Labels.Luck}{" "}
+                        </span>
+                        <span>{contextState.luck}</span>
+                      </Container>
                     </Container>
                   </Container>
-                  <Container
-                    sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
-                    id="armor-row"
-                    extraProps={{
-                      "data-tip": seeMore
-                        ? languageState.texts.CharacterPortrait.Tooltips.Armor
-                        : "",
-                    }}
-                  >
-                    <span className={label}>{AttributeIcons.armor}</span>
-                    <Container sx={{ marginBottom: "2px" }}>
-                      <span className={label}>
-                        {languageState.texts.CharacterPortrait.Labels.Armor}{" "}
-                      </span>
-                      <span>{contextState.armor}</span>
-                    </Container>
-                  </Container>
-                  <Container
-                    sx={{ ...portraitRow, ...bigFont }}
-                    id="luck-row"
-                    extraProps={{
-                      "data-tip": seeMore
-                        ? languageState.texts.CharacterPortrait.Tooltips.Luck
-                        : "",
-                    }}
-                  >
-                    <span style={{ marginRight: "20px" }}>
-                      {AttributeIcons.luck}
-                    </span>
-                    <Container sx={{ marginBottom: "2px" }}>
-                      <span className={label}>
-                        {languageState.texts.CharacterPortrait.Labels.Luck}{" "}
-                      </span>
-                      <span>{contextState.luck}</span>
-                    </Container>
-                  </Container>
-                </Container>
 
-                <Container flexDirection="column" sx={{ marginLeft: "20px" }}>
-                  <Container
-                    sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
-                    id="life-row"
-                    extraProps={{
-                      "data-tip": seeMore
-                        ? languageState.texts.CharacterPortrait.Tooltips.Life
-                        : "",
-                    }}
-                  >
-                    <span style={{ marginRight: "20px" }}>
-                      {AttributeIcons.life}
-                    </span>
-                    <Container sx={{ marginBottom: "2px" }}>
-                      <span className={label}>
-                        {languageState.texts.CharacterPortrait.Labels.Life}{" "}
+                  <Container flexDirection="column" sx={{ marginLeft: "20px" }}>
+                    <Container
+                      sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
+                      id="life-row"
+                      extraProps={{
+                        "data-tip": seeMoreAnimation
+                          ? languageState.texts.CharacterPortrait.Tooltips.Life
+                          : "",
+                      }}
+                    >
+                      <span style={{ marginRight: "20px" }}>
+                        {AttributeIcons.life}
                       </span>
-                      <span>
-                        {contextState.life.current} / {contextState.life.max}
-                      </span>
+                      <Container sx={{ marginBottom: "2px" }}>
+                        <span className={label}>
+                          {languageState.texts.CharacterPortrait.Labels.Life}{" "}
+                        </span>
+                        <span>
+                          {contextState.life.current} / {contextState.life.max}
+                        </span>
+                      </Container>
                     </Container>
-                  </Container>
-                  <Container
-                    sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
-                    id="mana-row"
-                    extraProps={{
-                      "data-tip": seeMore
-                        ? languageState.texts.CharacterPortrait.Tooltips.Mana
-                        : "",
-                    }}
-                  >
-                    <span className={label}>{AttributeIcons.mana}</span>
-                    <Container sx={{ marginBottom: "2px" }}>
-                      <span className={label}>
-                        {languageState.texts.CharacterPortrait.Labels.Mana}{" "}
-                      </span>
-                      <span>
-                        {contextState.mana.current} / {contextState.mana.max}
-                      </span>
+                    <Container
+                      sx={{ ...portraitRow, ...bigFont, ...spaceAround }}
+                      id="mana-row"
+                      extraProps={{
+                        "data-tip": seeMoreAnimation
+                          ? languageState.texts.CharacterPortrait.Tooltips.Mana
+                          : "",
+                      }}
+                    >
+                      <span className={label}>{AttributeIcons.mana}</span>
+                      <Container sx={{ marginBottom: "2px" }}>
+                        <span className={label}>
+                          {languageState.texts.CharacterPortrait.Labels.Mana}{" "}
+                        </span>
+                        <span>
+                          {contextState.mana.current} / {contextState.mana.max}
+                        </span>
+                      </Container>
                     </Container>
                   </Container>
                 </Container>
+                <h3>
+                  {languageState.texts.CharacterPortrait.Labels.Description}
+                </h3>
+
+                <textarea
+                  className={description}
+                  placeholder={
+                    languageState.texts.CharacterPortrait.Placeholders
+                      .Description
+                  }
+                />
               </Container>
-              <h3>
-                {languageState.texts.CharacterPortrait.Labels.Description}
-              </h3>
-
-              <textarea
-                className={description}
-                placeholder={
-                  languageState.texts.CharacterPortrait.Placeholders.Description
-                }
-              />
             </Container>
-          </Container>
+          )}
         </Container>
 
         <button
